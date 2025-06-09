abade6edd2d47a8657ce687b95d1c3bfd3cfba8a
Jason Chen <shchenchang@gmail.com>
2025-06-09 15:35:13 +0800

    Add VSCode launch configuration and refactor footnote handling in test.py

--- docs/source/test.py
+++ docs/source/test.py
@@ -38,18 +38,48 @@ def f3():
         m = re.match(pattern, f)
         if m:
             print(f)
-            footnote(f)
+            check_footnote(f)
 
 
-def footnote(filename):
+def check_footnote(filename):
     pattern = '[①②③④⑤⑥⑦⑧⑨]'
+    # pattern = '[②]'
     with open(filename, encoding='utf-8') as f:
         for s in f.readlines():
             m = re.search(pattern, s)
             if m:
                 print(s)
 
+
+def footnote(filename):
+    pattern = '[①②③④⑤⑥⑦⑧⑨]'
+
+    sentences = []
+    footnotes = []
     
+    with open(filename, encoding='utf-8') as f:
+        for s in f.readlines():
+            m = re.search(pattern, s)
+            if m:
+                # 检查匹配的pattern是否在行首
+                if m.start() == 0:
+                    # 在行首，替换为 .. [#] 并追加到footnotes
+                    new_line = re.sub(pattern, ' .. [#] ', s)
+                    footnotes.append(new_line)
+                else:
+                    # 不在行首，替换为 [#]_ 并追加到sentences
+                    new_line = re.sub(pattern, ' [#]_ ', s)
+                    sentences.append(new_line)
+            else:
+                # 没有匹配到pattern，追加到sentences
+                sentences.append(s)
+    
+    # 合并sentences和footnotes
+    all_lines = sentences + footnotes
+    
+    # 回写到文件
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writelines(all_lines)
 
 
 if __name__ == '__main__':
